c28a5749d57e4f8792f4ca8dfa6467bb9ba885a5
Nils Schikora <nschikora@ujam.com>
2020-03-28 18:33:44 +0100

    added wagner test

---
+++ wagner/precache-manifest.d6cbaa2c0d68b7255f4c0082f5f33ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec71de7cb3a99b8f82b9f9c27164a5fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5bf1827f676375f6626",
+    "url": "/static/css/main.e96399ea.chunk.css"
+  },
+  {
+    "revision": "1fcaf612f65db47bbd5e",
+    "url": "/static/js/2.8c587059.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c587059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5bf1827f676375f6626",
+    "url": "/static/js/main.5ae3fa7c.chunk.js"
+  },
+  {
+    "revision": "7328f3324fc05dc40a79",
+    "url": "/static/js/runtime-main.c940b95b.js"
+  }
+]);
