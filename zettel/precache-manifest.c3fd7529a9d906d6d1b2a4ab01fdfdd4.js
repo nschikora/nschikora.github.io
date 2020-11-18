57f66f34cc9d87d15f4454224d3a7dcf23f5ae00
Nils Schikora <nils@schikora-it.de>
2020-11-18 21:56:45 +0100

    Kassenzettel-gen

---
+++ zettel/precache-manifest.c3fd7529a9d906d6d1b2a4ab01fdfdd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11be7e4e7c3a62414af8645acb44d1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0907798ea71a4d9a0787",
+    "url": "/static/css/main.f3a41d43.chunk.css"
+  },
+  {
+    "revision": "59c43c2cd97dc1a75bbf",
+    "url": "/static/js/2.53370024.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53370024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0907798ea71a4d9a0787",
+    "url": "/static/js/main.b43e908f.chunk.js"
+  },
+  {
+    "revision": "68ccf877ce0a3e18ae49",
+    "url": "/static/js/runtime-main.635da551.js"
+  }
+]);
